d6a2a0cf459dae9b0fa0520f856d93969c7b4117
cinben18 <cinben18@hotmail.com>
2019-02-05 09:00:18 -0800

    log

---
+++ RollABall-Cavin004/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        setCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You win!";
+        }
+    }
+
+    void OnGUI()
+    {
+        int origin_x = Screen.width - 150;
+        int origin_y = 15;
+        if (GUI.Button(new Rect(origin_x, origin_y, 100, 50), "Main Menu"))
+        {
+            SceneManager.LoadScene("Main Screen");
+        }
+    }
+}
+
+
+
+
+
